337d6443874e57d2b6e477f46858d5d5598df6b7
Mehdi Serra <mehdi.serra57@gmail.com>
2021-08-24 19:54:02 +0200

    View pager refacto

---
+++ constants/OnBoardingContent.js
@@ -0,0 +1,20 @@
+import { ILLUSTRATION_1, ILLUSTRATION_2, ILLUSTRATION_3, ILLUSTRATION_4 } from '../constants/Images';
+
+export const ON_BOARDING_CONTENT = [
+    {
+        title: "Bienvenue dans Monumental Habits",
+        img: ILLUSTRATION_1
+    },
+    {
+        title: "Creez des nouvelles habitudes facilement",
+        img: ILLUSTRATION_2
+    },
+    {
+        title: "Gardez en oeil sur vos progres",
+        img: ILLUSTRATION_3
+    },
+    {
+        title: "Rejoignez une communaute inspirante",
+        img: ILLUSTRATION_4
+    },
+];
--- screens/OnBoarding.js
+++ screens/OnBoarding.js
@@ -2,37 +2,29 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import ViewPager from '@react-native-community/viewpager';
 import OnBoardingPage from '../components/OnBoardingPage';
-import { ILLUSTRATION_1, ILLUSTRATION_2, ILLUSTRATION_3, ILLUSTRATION_4 } from '../constants/Images';
+import { ON_BOARDING_CONTENT } from '../constants/OnBoardingContent';
 
 const OnBoarding = () => {
+
+    const renderPage = () => {
+        return (
+            <ViewPager style={styles.container}>
+                {ON_BOARDING_CONTENT.map((item, index) => (
+                    <View key={index}>
+                        <OnBoardingPage
+                            backgroundColor="#FFFFFF"
+                            img={item.img}
+                            title={item.title}
+                        />
+                    </View>
+                ))}
+            </ViewPager>
+        );
+    };
+
     return (
         <View style={styles.container}>
-            <ViewPager style={styles.container}>
-                <View key="1">
-                    <OnBoardingPage
-                        backgroundColor="#FFFFFF"
-                        img={ILLUSTRATION_1}
-                        title="Bienvenue dans Monumental Habits" />
-                </View>
-                <View key="2">
-                    <OnBoardingPage
-                        backgroundColor="#FFFFFF"
-                        img={ILLUSTRATION_2}
-                        title="Creez des nouvelles habitudes facilement." />
-                </View>
-                <View key="3">
-                    <OnBoardingPage
-                        backgroundColor="#FFFFFF"
-                        img={ILLUSTRATION_3}
-                        title="Gardez en oeil sur vos progres." />
-                </View>
-                <View key="4">
-                    <OnBoardingPage
-                        backgroundColor="#FFFFFF"
-                        img={ILLUSTRATION_4}
-                        title="Rejoignez une communaute inspirante." />
-                </View>
-            </ViewPager>
+            {renderPage()}
         </View>
     );
 }
